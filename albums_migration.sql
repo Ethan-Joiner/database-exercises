f7f39fd6e595e404c9777bd93326a48dcebab56f
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-18 11:44:28 -0600

    select complete

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
--- DROP TABLE IF EXISTS albums;
+DROP TABLE IF EXISTS albums;
 CREATE TABLE IF NOT EXISTS albums(
 id INT UNSIGNED AUTO_INCREMENT,
 artist VARCHAR(50),
 name VARCHAR(50),
-release_date DATE,
+release_date INT,
 sales FLOAT,
 genre VARCHAR(50),
 PRIMARY KEY (id)
