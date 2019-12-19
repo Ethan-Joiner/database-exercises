15b228fdab5224a8451a75cd47d4ef785bdcc2c1
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-19 16:58:39 -0600

    working on 4

--- join_exercises.sql
+++ join_exercises.sql
@@ -9,10 +9,27 @@ ON dm.emp_no = e.emp_no
 WHERE dm.to_date = '9999-01-01'
 ORDER BY d.dept_name;
 
+SELECT d.dept_name, CONCAT(e.first_name, ' ' , e.last_name)
+FROM departments AS d
+JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01' AND e.gender = 'F'
+ORDER BY d.dept_name;
+
+SELECT t.title, count(*)
+FROM titles as t
+JOIN dept_emp as de
+ON t.emp_no = de.emp_no
+JOIN departments as d
+ON de.dept_no = d.dept_no
+WHERE de.to_date = '9999-01-01' AND dept_name = 'Customer Service'
+GROUP BY t.title;
+
 show tables;
-SELECT * FROM dept_manager;
-SELECT from_date
-FROM dept_manager;
+select * from departments;
+
 
 
 
