63686c583ee5d720dc623ce1e63a1b08f1e56f2a
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-19 16:48:15 -0600

    current manager done

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,21 +1,46 @@
-USE join_test_db;
+USE employees;
 
-SHOW tables;
-DESCRIBE users;
-DESCRIBE roles;
-INSERT INTO users (name, email, role_id) VALUES
-('Dennis', 'theimplication@paddys.com', 2),
-('Mac', 'ducktowel@paddys.com', 2),
-('Charlie', 'pirate@paddys.com', null),
-('Dee', 'bird@paddys.com', 2);
-SELECT users.name, roles.name
-FROM users
-JOIN roles ON users.role_id = roles.id;
+SELECT d.dept_name, CONCAT(e.first_name, ' ' , e.last_name)
+FROM departments AS d
+JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no
+JOIN employees AS e
+ON dm.emp_no = e.emp_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY d.dept_name;
 
-SELECT users.name AS user_name, roles.name AS role_name
-FROM users
-LEFT JOIN roles ON users.role_id = roles.id;
+show tables;
+SELECT * FROM dept_manager;
+SELECT from_date
+FROM dept_manager;
 
-SELECT users.name AS user_name, roles.name AS role_name
-FROM users
-RIGHT JOIN roles ON users.role_id = roles.id;
+
+
+
+
+
+
+
+
+
+
+
+# SHOW tables;
+# DESCRIBE users;
+# DESCRIBE roles;
+# INSERT INTO users (name, email, role_id) VALUES
+# ('Dennis', 'theimplication@paddys.com', 2),
+# ('Mac', 'ducktowel@paddys.com', 2),
+# ('Charlie', 'pirate@paddys.com', null),
+# ('Dee', 'bird@paddys.com', 2);
+# SELECT users.name, roles.name
+# FROM users
+# JOIN roles ON users.role_id = roles.id;
+#
+# SELECT users.name AS user_name, roles.name AS role_name
+# FROM users
+# LEFT JOIN roles ON users.role_id = roles.id;
+#
+# SELECT users.name AS user_name, roles.name AS role_name
+# FROM users
+# RIGHT JOIN roles ON users.role_id = roles.id;
