0c1301d694d1ad264f1c1c59125cc982d48631ba
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-19 11:13:01 -0600

    bonus two completed

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -25,3 +25,25 @@ WHERE first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name= 'Maya'
 GROUP BY gender;
+
+# Bonuses
+# what are the names of the 10 most recently hired females in the company? the first 10? (no group by needed)
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE gender = 'F'
+ORDER BY hire_date DESC
+LIMIT 10;
+# what are the top three most common job titles?
+SELECT title, count(*)
+FROM titles
+GROUP BY title
+ORDER BY count(*) DESC
+LIMIT 3;
+# what is the most common 'from date' for job titles? for staff only?
+# what is the highest employee number for an engineer?
+# what is the most common birthday?
+# what is the most common female birthday? male?
+# what is the most common hire date for female and male employees?
+# what is the longest last name of someone born on March 8, 1952?
+# what is the lowest employee number for the first senior engineers in the company?
+
