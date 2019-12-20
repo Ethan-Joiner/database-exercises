ad0c593b982b8b2bdc53dafc68d00f095f637ffe
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-20 15:14:47 -0600

    3rd problem complete

--- relationship_drills.sql
+++ relationship_drills.sql
@@ -16,13 +16,40 @@ SELECT CONCAT((
     LIMIT 1 OFFSET 1));
 
 SELECT MAX(salary) FROM salaries GROUP BY emp_no ORDER BY emp_no LIMIT 2;
-SELECT MAX(salary) FROM salaries WHERE emp_no = '100002'
+SELECT MAX(salary) FROM salaries WHERE emp_no = '100002';
 SELECT MAX(salary)
 FROM salaries
 GROUP BY emp_no
 ORDER BY emp_no
 LIMIT 1 OFFSET 1;
 -- get current manager name of Sales department (use subqueries)
+SELECT CONCAT(first_name, ' ', last_name) FROM employees
+WHERE emp_no = (
+    SELECT emp_no
+    FROM dept_manager
+    WHERE to_date > NOW() AND dept_no = (
+        SELECT dept_no
+        FROM departments
+        WHERE dept_name = 'Sales'
+        )
+    );
+
+SELECT * FROM departments;
 -- get all senior engineers in customer service department
+SELECT * FROM employees
+WHERE emp_no IN (
+    SELECT emp_no
+    FROM dept_emp
+    WHERE dept_no IN (
+        SELECT dept_no
+        FROM departments
+        WHERE dept_name = 'Customer Service'
+        ) AND emp_no IN (
+            SELECT emp_no
+            FROM titles
+            WHERE title = 'Senior Engineer'
+        )
+
+          );
 -- get all first and last names of current Sales department employees
 -- get the name of the employee with the highest salary
