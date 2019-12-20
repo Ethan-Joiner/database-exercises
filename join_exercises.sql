1a6b3d4af133ab74bb3754cc4e5020846131b44a
Ethan Joiner <ethan.joiner@gmail.com>
2019-12-20 10:19:08 -0600

    join bonus complete

--- join_exercises.sql
+++ join_exercises.sql
@@ -24,7 +24,7 @@ JOIN dept_emp as de
 ON t.emp_no = de.emp_no
 JOIN departments as d
 ON de.dept_no = d.dept_no
-WHERE de.to_date = '9999-01-01' AND dept_name = 'Customer Service'
+WHERE de.to_date = '9999-01-01' AND dept_name = 'Customer Service' AND t.to_date = '9999-01-01'
 GROUP BY t.title;
 
 SELECT d.dept_name, CONCAT(e.first_name, ' ', e.last_name), s.salary
@@ -39,11 +39,28 @@ WHERE dm.to_date = '9999-01-01' AND s.to_date = '9999-01-01'
 GROUP BY d.dept_name, CONCAT(e.first_name, ' ', e.last_name), s.salary
 ORDER BY dept_name;
 
+SELECT CONCAT(e.first_name, ' ', e.last_name), d.dept_name, CONCAT(em.first_name, ' ', em.last_name)
+FROM employees AS e
+JOIN dept_emp AS de
+ON e.emp_no = de.emp_no
+JOIN departments AS d
+ON de.dept_no = d.dept_no
+JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no
+JOIN employees AS em
+ON em.emp_no = dm.emp_no
+WHERE dm.to_date = '9999-01-01' AND de.to_date = '9999-01-01'
+# GROUP BY CONCAT(e.first_name, ' ', e.last_name), d.dept_name, CONCAT(em.first_name, ' ', em.last_name)
+ORDER BY CONCAT(e.first_name, ' ', e.last_name);
+
+
+
 
 show tables;
 select * from departments;
-select * from salaries
-LIMIT 1;
+select * from salaries;
+select * from dept_manager;
+
 
 
 
