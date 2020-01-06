12f4cb4c1acb74ccda81f08068b9fee87b5db3e5
Ethan Joiner <ethan.joiner@gmail.com>
2020-01-06 09:28:02 -0600

    migration complete

--- sporkify_migration.sql
+++ sporkify_migration.sql
@@ -1,12 +1,13 @@
 
-
+USE sporkify_db;
 DROP TABLE IF EXISTS sporkify;
 CREATE TABLE IF NOT EXISTS sporkify(
                                      id INT UNSIGNED AUTO_INCREMENT,
                                      artist VARCHAR(50),
-                                     name VARCHAR(50),
                                      album VARCHAR(50),
-                                     release_date INT,
+                                     song VARCHAR(50),
+                                     genre VARCHAR(50),
                                      duration INT,
+                                     release_date DATE,
                                      PRIMARY KEY (id)
 );
